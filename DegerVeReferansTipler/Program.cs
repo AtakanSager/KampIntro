ada2270fb1da7fd44f8e97d41bed44d21fe8ca04
ataka <ataka@DESKTOP-LPFCC6V>
2023-04-04 15:28:49 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        int sayi1 = 10;
+        int sayi2 = 30;
+        sayi1 = sayi2;
+        sayi2 = 
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string kurs1 = "Yazılım geliştirici yetiştirme kampı ";
+
+
+        //array - dizi
+
+        string[] kurslar = new string[] { "Yazılım geliştirici yetiştirme kampı", 
+            "Programlamaya başlangıç için temel kurs" ,"Java", "Python"};
+        for (int i = 0; i < kurslar.Length; i++)  
+        {
+            Console.WriteLine(kurslar[i]);
+        }
+
+        Console.WriteLine("For bitti");
+
+        foreach (string kurs in kurslar) 
+        {
+            Console.WriteLine(kurs);
+        }
+        Console.WriteLine("sayfa sonu - footer");
+    }
+}
